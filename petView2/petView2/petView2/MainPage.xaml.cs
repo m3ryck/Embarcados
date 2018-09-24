b6df5556195e2c2afae29ca1d46ab6f0224c7b2e
m3ryck <adriano.bsanto@eng.ci.ufpb.br>
2018-09-24 00:19:54 -0300

    ARQUIVO CERTO

---
+++ petView2/petView2/petView2/MainPage.xaml.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+using petView2;
+using petView2.MONITOR;
+using petView2.ALIMENTADOR;
+using petView2.SERVICE;
+using Newtonsoft.Json.Linq;
+
+namespace petView2
+{
+	public partial class MainPage : ContentPage
+	{
+      
+		public MainPage()
+		{
+            InitializeComponent();
+            
+        }
+
+        private void Monitor(object sender, EventArgs e)
+        {
+            Navigation.PushAsync(new webVieww());
+        }
+
+        private void Slider_ValueChanged(object sender, ValueChangedEventArgs e)
+        {
+            //slider.Value = Math.Round(e.NewValue);
+
+            var newStep = Math.Round(e.NewValue);
+            slider.Value = newStep;
+            lblText.Text = slider.Value.ToString();
+            //lblText.TranslateTo(slider.Value * ((slider.Width) / slider.Maximum), 0, 100);
+        }
+
+        private void Agendar_Alimento()
+        {
+            double param = (slider.Value)*(2000);
+
+            JObject user = new JObject();
+
+            user.Add("action", "write");
+            user.Add("gpio", 17);
+            user.Add("status", true);
+            user.Add("tempo", param);
+
+            ServiceWS.AtivarAlimentador(user);
+            
+        }
+
+       
+    }
+}
